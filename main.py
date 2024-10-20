1a57e17243e96fefb1d8fbbddf8614d3d33be24f
StrixROX <40167899+StrixROX@users.noreply.github.com>
2024-10-20 19:37:12 +0530

    Added code to export data to json files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from os import path
+import json
 
 from Scrapers.sonata import Sonatas
 from Scrapers.item import Items
@@ -10,11 +11,28 @@ from Scrapers.character import Characters
 
 ICON_ROOT_PATH = str(path.abspath('./assets/'))
 DATA_ROOT_PATH = str(path.abspath('./data/'))
+OUTPUT_ROOT_PATH = str(path.abspath('./output/'))
 
-sonatas = Sonatas.fetch(icons=True, iconRootPath=ICON_ROOT_PATH)
-items = Items.fetch(icons=True, iconRootPath=ICON_ROOT_PATH)
-enemies = Enemies.fetch(icons=True, iconRootPath=ICON_ROOT_PATH)
-echoes = Echoes.fetch(icons=True, iconRootPath=ICON_ROOT_PATH) # all echo icons are same as enemy icons
-weaponTypes = WeaponTypes.fetch(icons=True, iconRootPath=ICON_ROOT_PATH)
-attributes = Attributes.fetch(icons=True, iconRootPath=ICON_ROOT_PATH)
-characters = Characters.fetch(cards=True, icons=False, iconRootPath=ICON_ROOT_PATH, dataRootPath=DATA_ROOT_PATH)
+DOWNLOAD_IMAGES = False
+
+sonatas = Sonatas.fetch(icons=DOWNLOAD_IMAGES, iconRootPath=ICON_ROOT_PATH)
+items = Items.fetch(icons=DOWNLOAD_IMAGES, iconRootPath=ICON_ROOT_PATH)
+enemies = Enemies.fetch(icons=DOWNLOAD_IMAGES, iconRootPath=ICON_ROOT_PATH)
+echoes = Echoes.fetch(icons=DOWNLOAD_IMAGES, iconRootPath=ICON_ROOT_PATH) # all echo icons are same as enemy icons
+weaponTypes = WeaponTypes.fetch(icons=DOWNLOAD_IMAGES, iconRootPath=ICON_ROOT_PATH)
+attributes = Attributes.fetch(icons=DOWNLOAD_IMAGES, iconRootPath=ICON_ROOT_PATH)
+characters = Characters.fetch(cards=DOWNLOAD_IMAGES, icons=DOWNLOAD_IMAGES, iconRootPath=ICON_ROOT_PATH, dataRootPath=DATA_ROOT_PATH)
+
+outputFilePaths = {
+  "sonatas.json": [x.export() for x in sonatas],
+  "items.json": [x.export() for x in items],
+  "enemies.json": [x.export() for x in enemies],
+  "echoes.json": [x.export() for x in echoes],
+  "weaponTypes.json": [x.export() for x in weaponTypes],
+  "attributes.json": [x.export() for x in attributes],
+  "characters.json": [x.export() for x in characters]
+}
+
+for i in outputFilePaths:
+  with open(path.join(OUTPUT_ROOT_PATH, i), "w") as f:
+    f.write(json.dumps(outputFilePaths[i]))
